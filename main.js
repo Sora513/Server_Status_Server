c5cda57ac0ff7506350a91bca16128a7a06ab7f5
Sora513 <kokubo.tech@gmail.com>
2020-01-30 21:23:19 +0900

    fix

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // nodeのコアモジュールのhttpを使う
 var http = require("http");
-var data
-
+var data = "n"
 http.createServer(function (req, res) {
     if (req.method === 'GET') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('Received');
         res.end();
     } else if (req.method === 'POST') {
+	    data = "n"
         //POSTデータを受信
         req.on('data', function (chunk) { data += chunk })
             .on('end', function () {
-
-                console.log(data);
+		//data = JSON.parse(data);
+                data = JSON.parse(data.slice(1));
                 res.end();
-
+		console.log(data.Name)
             })
     }
 }).listen(3000);
